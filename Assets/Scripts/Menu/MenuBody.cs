e6754d9acc3e5d6c70553ef20525899a06c59d4d
k3nsalazar2012 <31090803+k3nsalazar2012@users.noreply.github.com>
2021-01-07 17:48:50 +0800

    Menu layout placeholder

---
+++ Assets/Scripts/Menu/MenuBody.cs
@@ -0,0 +1,39 @@
+﻿
+using UnityEngine;
+
+using DG.Tweening;
+
+namespace Jollibee.Menu
+{
+    public class MenuBody : MonoBehaviour
+    {
+        [SerializeField] private bool _isCenter = false;
+
+        private RectTransform _rectTransform = null;
+        private float _screenWidth = 0f;
+
+        private void Awake()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+            _screenWidth = _rectTransform.rect.size.x;
+
+            if (_isCenter)
+                _rectTransform.DOAnchorPosX(0f, 0f);
+            else
+                _rectTransform.DOAnchorPosX(_screenWidth, 0f);
+        }
+
+        public void MoveToLeft(float duration)
+        {
+            _rectTransform.DOAnchorPosX(Position - _screenWidth, duration);
+        }
+
+        public void MoveToRight(float duration)
+        {
+            _rectTransform.DOAnchorPosX(Position + _screenWidth, duration);
+        }
+
+        public float Position { get { return _rectTransform.anchoredPosition.x; } }
+        public RectTransform Canvas { get { return _rectTransform; } }
+    }
+}
---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,69 @@
+﻿// c# / unity class
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+// third party class
+using DG.Tweening;
+using System;
+
+namespace Jollibee.Menu
+{
+    public class MenuManager : MonoBehaviour
+    {
+        [SerializeField] private Color _activeColor = Color.white;
+        [SerializeField] private Color _defaultColor = Color.white;
+
+        [SerializeField] private Button _currentMenuButton = null;
+        [SerializeField] private MenuBody[] _menuScreens = null;
+
+        private MenuBody _currentScreen = null;
+        private MenuBody _previousScreen = null;
+        private float _transitionDuration = 0.25f;
+
+        private void Start()
+        {
+            _currentScreen = _menuScreens[0];
+            ChangeColor(_currentMenuButton, _activeColor);
+        }
+
+        public void MenuButtonClick(Button button)
+        {
+            if (_currentMenuButton == button) return;
+
+            ChangeColor(_currentMenuButton, _defaultColor);
+            ChangeColor(button, _activeColor);
+
+            int index = button.transform.GetSiblingIndex();
+            _previousScreen = _currentScreen;
+            _currentScreen = _menuScreens[index];
+            SwitchScreen();
+        }
+
+        private void ChangeColor(Button targetButton, Color newColor)
+        {
+            targetButton.GetComponent<Image>().DOColor(newColor, _transitionDuration);
+
+            if(newColor == _activeColor)
+                targetButton.GetComponentInChildren<TMP_Text>().DOColor(newColor, _transitionDuration).OnComplete(()=> OnColorComplete(targetButton));
+            else
+                targetButton.GetComponentInChildren<TMP_Text>().DOColor(newColor, _transitionDuration);
+        }
+
+        private void SwitchScreen()
+        {
+            if (_previousScreen.Position < _currentScreen.Position)
+            {
+                _previousScreen.MoveToLeft(_transitionDuration);
+                _currentScreen.MoveToLeft(_transitionDuration);
+            }
+            else
+            {
+                _previousScreen.MoveToRight(_transitionDuration);
+                _currentScreen.MoveToRight(_transitionDuration);
+            }
+        }
+
+        private void OnColorComplete(Button button) => _currentMenuButton = button;
+    }
+}
